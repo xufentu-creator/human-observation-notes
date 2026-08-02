d674dfe85ea0e616c268cbda0dffa47d73da005e
xufentu <246942485+xufentu-creator@users.noreply.github.com>
2026-08-02 13:02:37 -0700

    feat: add public observation reading site v1.1.1

---
+++ site/config.js
@@ -0,0 +1,9 @@
+window.SITE_CONFIG = {
+  owner: 'xufentu-creator',
+  repo: 'human-observation-notes',
+  branch: 'main',
+  contentDir: 'daily',
+  siteTitle: 'Human Observation Notes',
+  author: 'Xufen Tu',
+  siteUrl: 'https://observations.xufentu.com'
+};
